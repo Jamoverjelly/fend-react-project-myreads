2f16369bc2c3f518c687d326506cf59decd4816f
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-11-18 16:23:36 -0500

    Updated project with necessary dependencies.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-class App extends Component {
+import SearchPage from './searchPage'
+import MainPage from './mainPage'
+
+import * as BooksAPI from './BooksAPI'
+import { Route } from 'react-router-dom'
+
+class BooksApp extends React.Component {
+  state = {
+    // array container for holding dynamic state data for books
+    booksList: []
+  }
+
+  // perform fetch once component has completed initial render
+  componentDidMount() {
+    // retrieving book data from API returns Promise
+    BooksAPI.getAll().then((books) => {
+      // update state for booksList, triggers re-render
+      this.setState({ booksList: books })
+    })
+  }
+
+  // create method for changing shelf status of book on both the main page and search page
+  updateShelf = (book, shelf) => {
+    // utilizing update method from BooksAPI, pass in parameters
+    BooksAPI.update(book, shelf);
+
+    // Re-run fetch request for books data, update state of booksList
+    BooksAPI.getAll().then((books) => {
+      this.setState({ booksList: books })
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        {/* route for rendering main page */}
+        <Route exact path='/' render={() => (
+          <MainPage
+            // booksList state can be passed to MainPage component as immutable props, books
+            books={this.state.booksList}
+            // provide access to updateShelf method in MainPage component, passing data down through children components
+            updateShelf={this.updateShelf}
+          />
+        )}/>
+        {/* route for rendering search page */}
+        <Route path='/search' render={() => (
+          <SearchPage
+            // provide access to updateShelf method in SearchPage component
+            updateShelf={this.updateShelf}
+            // send current state of booksList to SearchPage component
+            booksList={this.state.booksList}
+          />
+        )}/>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default BooksApp
