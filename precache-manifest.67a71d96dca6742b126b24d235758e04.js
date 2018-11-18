df62d8b12bf4199039bdb5c2a2de82072577ac2f
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-11-18 16:35:34 -0500

    Updates

---
+++ precache-manifest.67a71d96dca6742b126b24d235758e04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/fend-react-project-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/fend-react-project-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/fend-react-project-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "4a6e7d69f97b1866aa96",
+    "url": "/fend-react-project-myreads/static/js/runtime~main.4a6e7d69.js"
+  },
+  {
+    "revision": "0f976223c73869b47db8",
+    "url": "/fend-react-project-myreads/static/js/main.0f976223.chunk.js"
+  },
+  {
+    "revision": "454a98a5f941fd3dd2c9",
+    "url": "/fend-react-project-myreads/static/js/1.454a98a5.chunk.js"
+  },
+  {
+    "revision": "0f976223c73869b47db8",
+    "url": "/fend-react-project-myreads/static/css/main.247c89f9.chunk.css"
+  },
+  {
+    "revision": "69ae395f12e7f05b137b9d10d67f639d",
+    "url": "/fend-react-project-myreads/index.html"
+  }
+];
