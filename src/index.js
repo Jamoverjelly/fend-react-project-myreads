8abee7dc6256e2c672ec6386f7d6bc7b0f33b250
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-11-18 16:58:08 -0500

    Updated BrowserRouter component in index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <BrowserRouter basename="/fend-react-project-myreads"><App /></BrowserRouter>,
   document.getElementById('root')
 );
 
